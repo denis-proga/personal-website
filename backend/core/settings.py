442b8bfc9a7b94bbb9c32cb374340e2ffe429830
Denys Peresunko <peresunkodenis11@gmail.com>
2026-08-26 23:06:02 +0200

    Add Render host to ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1u14s*)&$x8^l@um2r(=mmbmp#y0shfwi&c2$m$==_+xsr-%hy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personal-website-p8el.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
